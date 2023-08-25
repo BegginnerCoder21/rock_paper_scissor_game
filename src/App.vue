7a2979240201d6fe4eefd99b63fc6aab2eb54f23
Teguera <aboubacarteguera21@gmail.com>
2023-08-25 23:48:51 +0200

    debut du projet

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Home from '@/views/Home.vue';
+</script>
+
+<template>
+  <main>
+      <Home />
+  </main>
+</template>
+
+<style>
+* {
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
